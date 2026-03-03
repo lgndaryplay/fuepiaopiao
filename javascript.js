c62d72146ca17850b1ec8398a2171d302d148b0d
lgndaryplay <29bryson.darling@boiseschools.net>
2026-03-02 22:58:11 -0700

    Add welcome function to greet users

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
 
+// function to welcome the user when they click the button
+function welcomeHooper() {
+    // ask for the user's name
+    let person = prompt("Welcome to the court! What's your name?");
+
+    // logic to check if they entered a name
+    if (person != null && person != "") {
+        alert("Nice to meet you, " + person + "! Grab a ball and let's get to work.");
+        console.log("New team member added: " + person);
+    } else {
+        alert("Welcome, Anonymous Baller!");
+    }
+}
